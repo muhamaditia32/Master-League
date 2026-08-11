3cca976dd8f904865210c91dc3b95c57f853df94
muhamaditia32 <muhamaditia32@gmail.com>
2026-08-11 08:44:33 +0700

    Create players.js

---
+++ data/players.js
@@ -0,0 +1,39 @@
+const players = [
+
+    {
+        id: "player-001",
+
+        name: "Example Player",
+
+        age: 20,
+
+        nationality: "England",
+
+        position: "CM",
+
+        overall: 75,
+
+        potential: 82,
+
+        marketValue: 25000000,
+
+        salary: 50000,
+
+        preferredFoot: "Right",
+
+        height: 180,
+
+        club: "example-club",
+
+        attributes: {
+            pace: 70,
+            shooting: 65,
+            passing: 78,
+            dribbling: 74,
+            defending: 60,
+            physical: 72
+        }
+
+    }
+
+];
