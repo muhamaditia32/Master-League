46c1a212b3c6f7a92bba1dbf30701d4dbee98530
muhamaditia32 <muhamaditia32@gmail.com>
2026-08-11 08:21:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function newCareer() {
+    alert("NEW CAREER akan segera dibuat!");
+}
 
+function continueGame() {
+    alert("Belum ada career yang tersimpan.");
+}
+
+function loadGame() {
+    alert("Belum ada save game.");
+}
+
+function settings() {
+    alert("Settings akan segera tersedia.");
+}
